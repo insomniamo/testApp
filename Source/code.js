088dc6944d648b5442dd00997537335ccc2cf5d2
Sergey <warriorfromwar@gmail.com>
2023-09-13 01:59:14 +0300

    fix

--- Source/code.js
+++ Source/code.js
@@ -1,75 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const productInput = document.getElementById("product-input");
-    const addProductBtn = document.getElementById("add-product-btn");
-    const clearProductsBtn = document.getElementById("clear-products-btn");
-    const productList = document.getElementById("product-list");
+// app.js
 
-    // Загрузка продуктов из локального хранилища
-    const savedProducts = JSON.parse(localStorage.getItem("products")) || [];
-
-    // Функция для сохранения продуктов в локальное хранилище
-    function saveProducts() {
-        localStorage.setItem("products", JSON.stringify(savedProducts));
-    }
-
-    // Функция для отображения списка продуктов
-    function displayProducts() {
-        productList.innerHTML = "";
-        const completedProducts = savedProducts.filter(product => product.completed);
-        const uncompletedProducts = savedProducts.filter(product => !product.completed);
-
-        uncompletedProducts.forEach((product) => {
-            const li = createProductListItem(product);
-            productList.appendChild(li);
-        });
-
-        completedProducts.forEach((product) => {
-            const li = createProductListItem(product);
-            li.classList.add("completed");
-            productList.appendChild(li);
-        });
-    }
-
-    // Функция для создания элемента списка продуктов
-    function createProductListItem(product) {
-        const li = document.createElement("li");
-        li.textContent = product.name;
-        li.addEventListener("click", () => {
-            product.completed = !product.completed;
-            saveProducts();
-            displayProducts();
-        });
-        return li;
-    }
-
-    // Обработчик клика по кнопке "Добавить"
-    addProductBtn.addEventListener("click", addProduct);
-
-    // Обработчик клика по кнопке "Очистить список"
-    clearProductsBtn.addEventListener("click", () => {
-        savedProducts.length = 0; // Очищаем массив продуктов
-        saveProducts();
-        displayProducts();
-    });
-
-    // Обработчик нажатия клавиши "Enter" в поле ввода продукта
-    productInput.addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
-            addProduct();
-        }
-    });
-
-    // Функция для добавления продукта
-    function addProduct() {
-        const productName = productInput.value.trim();
+new Vue({
+    el: "#app",
+    data: {
+      newProduct: "",
+      products: JSON.parse(localStorage.getItem("products")) || [],
+    },
+    methods: {
+      addProduct() {
+        const productName = this.newProduct.trim();
         if (productName) {
-            savedProducts.push({ name: productName, completed: false });
-            saveProducts();
-            productInput.value = "";
-            displayProducts();
+          this.products.push({ name: productName, completed: false });
+          this.saveProducts();
+          this.newProduct = "";
         }
-    }
-
-    // Инициализация отображения списка продуктов
-    displayProducts();
-});
+      },
+      clearProducts() {
+        this.products = [];
+        this.saveProducts();
+      },
+      toggleComplete(product, index) {
+        if (product.completed) {
+          // Если продукт был зачеркнут, делаем его незачеркнутым
+          product.completed = false;
+          // Перемещаем продукт в начало списка
+          this.products.splice(index, 1);
+          this.products.unshift(product);
+        } else {
+          // Если продукт был незачеркнутым, делаем его зачеркнутым
+          product.completed = true;
+          // Перемещаем продукт в конец списка
+          this.products.splice(index, 1);
+          this.products.push(product);
+        }
+        this.saveProducts();
+      },
+      saveProducts() {
+        localStorage.setItem("products", JSON.stringify(this.products));
+      },
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.newProduct = ""; // Сбрасываем поле ввода при загрузке приложения
+      });
+    },
+  });
